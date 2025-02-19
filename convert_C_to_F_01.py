e4d0a59b468af992eddff4f9614bf4ae5d3a9eea
daveflcc <143441096+daveflcc@users.noreply.github.com>
2025-02-18 23:51:00 -0500

    Update convert_C_to_F_01.py

--- convert_C_to_F_01.py
+++ convert_C_to_F_01.py
@@ -1,5 +1,5 @@
 # FILE NAME - convert_C_to_F_01.py
-
+# DRG - Rerun for points 2025-02-18-2351
 # NAME: Michael Orcutt
 # DATE: 2/14/2025
 # BRIEF DESCRIPTION: A program to convert Celsius to Farenheit 
